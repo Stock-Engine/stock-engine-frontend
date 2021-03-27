dbae69b3423b9682eab14a0b2f9787887e4a438e
Adrian Naruszko <naruszko.adrian@gmail.com>
2021-03-27 20:36:16 +0100

    Add api with logging functionality

---
+++ api/api.py
@@ -0,0 +1,30 @@
+import flask
+import flask_praetorian
+from app import guard
+from flask import Blueprint, request
+
+api_blueprint = Blueprint(name="api", import_name=__name__, url_prefix="/api")
+
+
+@api_blueprint.route("/login", methods=["POST"])
+def login():
+    req = flask.request.get_json(force=True)
+    username = req.get("username", None)
+    password = req.get("password", None)
+    user = guard.authenticate(username, password)
+    ret = {"access_token": guard.encode_jwt_token(user)}
+    return ret, 200
+
+
+@api_blueprint.route("/refresh", methods=["POST"])
+def refresh():
+    old_token = request.get_data()
+    new_token = guard.refresh_jwt_token(old_token)
+    ret = {"access_token": new_token}
+    return ret, 200
+
+
+@api_blueprint.route("/dashboard", methods=["GET"])
+@flask_praetorian.auth_required
+def dashboard():
+    return "Dashboard"
---
+++ api/app.py
@@ -0,0 +1,43 @@
+import os
+
+import flask
+import flask_cors
+import flask_praetorian
+import flask_sqlalchemy
+
+guard = flask_praetorian.Praetorian()
+cors = flask_cors.CORS()
+db = flask_sqlalchemy.SQLAlchemy()
+
+
+def init_app():
+    app = flask.Flask(__name__)
+    app.config["SECRET_KEY"] = os.environ["FLASK_SECRET_KEY"]
+    app.config["JWT_ACCESS_LIFESPAN"] = {"hours": 24}
+    app.config["JWT_REFRESH_LIFESPAN"] = {"days": 30}
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["FLASK_DB_URI"]
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    from models import User
+
+    guard.init_app(app, User)
+    db.init_app(app)
+    cors.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
+    return app
+
+
+app = init_app()
+
+
+# Api makes import statements from app, it cannot be imported at the top of the file
+from api import api_blueprint  # noqa
+
+app.register_blueprint(api_blueprint)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("FLASK_PORT", 5000)))
