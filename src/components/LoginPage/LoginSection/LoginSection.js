b1ff721a37c634e611ca9703036bc4dd90b2f6d8
Olaf <olaf.naruszko@gmail.com>
2021-04-09 18:06:37 +0200

    Fix linter errors

--- src/components/LoginPage/LoginSection/LoginSection.js
+++ src/components/LoginPage/LoginSection/LoginSection.js
@@ -1,43 +1,43 @@
 import './LoginSection.css'
 
 function Login () {
-    return (
-        <div className='login'>
+  return (
+    <div className='login'>
 
-            <header className='login__section--header'>
-                <p className='header__logo'>
-                    <span>Stock</span> Engine
-                </p>
-            </header>
+      <header className='login__section--header'>
+        <p className='header__logo'>
+          <span>Stock</span> Engine
+        </p>
+      </header>
 
-            <div className='login__section'>
-                <section className='login__section--title'>
-                    <h2>Sign in</h2>
-                    <hr/>
-                </section>
+      <div className='login__section'>
+        <section className='login__section--title'>
+          <h2>Sign in</h2>
+          <hr />
+        </section>
 
-                <section className='login__section--form'>
-                    <form>
-                        <div className='form__input'>
-                            <span className='fa fa-user-o'></span>
-                            <input type="name" id="input--username" placeholder="Username" required></input>
-                        </div>
-                        <div className='form__input'>
-                            <span className='fa fa-key'></span>
-                            <input type="password" id="input--password" placeholder="Password" required></input>
-                        </div>
-                        <button type="submit" id="button--submit">Sign in</button>
-                    </form>
-                </section>
-
-                <section className='login__section--arrow'>
-                    <p>Don't have account?</p>
-                    <a href="#register__section"> </a>
-                </section>
+        <section className='login__section--form'>
+          <form method='post' action='../../../../api/api.py/post'>
+            <div className='form__input'>
+              <span className='fa fa-user-o' />
+              <input type='name' id='input--username' placeholder='Username' required />
             </div>
+            <div className='form__input'>
+              <span className='fa fa-key' />
+              <input type='password' id='input--password' placeholder='Password' required />
+            </div>
+            <button type='submit' id='button--submit'>Sign in</button>
+          </form>
+        </section>
 
-        </div>
-    )
+        <section className='login__section--arrow'>
+          <p>Don't have account?</p>
+          <a href='#register__section'> </a>
+        </section>
+      </div>
+
+    </div>
+  )
 }
 
 export default Login
--- src/components/LoginPage/RegisterSection/RegisterSection.js
+++ src/components/LoginPage/RegisterSection/RegisterSection.js
@@ -1,29 +1,29 @@
 import './RegisterSection.css'
 
 function Register () {
-    return (
-        <div className='register' id="register__section">
-            <section className='register__section--header'>
-                <h2>New to Stock Engine?</h2>
-                <hr/>
-                <p>Register and start using today!</p>
-            </section>
-            <section className='register__section--form'>
-                 <p>
-                    Our app works in demo mode,
-                    but if you want to test Stock Engine please write your email in the box below.
-                    We will contact with you as soon as possible!
-                </p>
-                <form>
-                     <div className='form__input--register'>
-                        <span className='fa fa-envelope-o'></span>
-                        <input type="email" id="input--email" placeholder="Email" required></input>
-                    </div>
-                    <button type="submit" id="button--register">Register</button>
-                </form>
-            </section>
-        </div>
-    )
+  return (
+    <div className='register' id='register__section'>
+      <section className='register__section--header'>
+        <h2>New to Stock Engine?</h2>
+        <hr />
+        <p>Register and start using today!</p>
+      </section>
+      <section className='register__section--form'>
+        <p>
+          Our app works in demo mode,
+          but if you want to test Stock Engine please write your email in the box below.
+          We will contact with you as soon as possible!
+        </p>
+        <form>
+          <div className='form__input--register'>
+            <span className='fa fa-envelope-o' />
+            <input type='email' id='input--email' placeholder='Email' required />
+          </div>
+          <button type='submit' id='button--register'>Register</button>
+        </form>
+      </section>
+    </div>
+  )
 }
 
 export default Register
