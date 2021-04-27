7ac92d5229899554a308f2560748eaa9db5a3064
Adrian Naruszko <naruszko.adrian@gmail.com>
2021-04-27 11:13:43 +0200

    Make LoginSection use backend to authenticate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css'
 import Login from './components/LoginPage/LoginSection/LoginSection'
 import Register from './components/LoginPage/RegisterSection/RegisterSection'
+import setToken from './components/LoginPage/LoginSection/token'
 
 function App () {
   return (
     <div className='App'>
       <div className='login--page'>
-        <Login />
+        <Login setToken={ setToken } />
         <Register />
       </div>
     </div>
