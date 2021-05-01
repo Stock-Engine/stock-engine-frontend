85dc6cd62cb00ec88fbe5a32cec03590c36e368a
Adrian Naruszko <naruszko.adrian@gmail.com>
2021-05-01 14:11:57 +0200

    Add main page with mocked data

---
+++ src/components/Queries/Queries.js
@@ -0,0 +1,74 @@
+import React from "react";
+import {withCookies} from "react-cookie";
+import {API} from "../../Api";
+
+
+class QueryInput extends React.Component {
+    render() {
+        return (
+            <form>
+                <label>
+                    Query:
+                    <input type="text" name="query" />
+                </label>
+                <input type="submit" value="Send" />
+            </form>
+        );
+    }
+}
+
+export class QueryList extends React.Component {
+    render() {
+        return (
+            <ul>
+                {
+                    this.props.data.map(el =>
+                        <li key={el.id}>
+                            { this.renderElement(el) }
+                        </li>
+                    )
+                }
+            </ul>
+        )
+    }
+
+    renderElement(el) {
+        return null;
+    }
+}
+
+class QueryHistoryList extends QueryList {
+    renderElement(el) {
+        return (
+            "Query " + el.name
+        );
+    }
+}
+
+class Queries extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { queryHistory: [] }
+    }
+
+    componentDidMount() {
+        const ths = this;
+
+        const { cookies } = this.props;
+        API.getQueryHistory(cookies)
+            .then((queries) => ths.setState({ queryHistory: queries}))
+    }
+
+    render() {
+        return (
+            <div>
+                <QueryInput />
+                <QueryHistoryList data={this.state.queryHistory} />
+            </div>
+
+        )
+    }
+}
+
+export default withCookies(Queries);
