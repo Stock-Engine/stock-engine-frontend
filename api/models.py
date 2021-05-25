a30f4eb3575ffddfa544c988c7a13de45332ae1e
Adrian Naruszko <naruszko.adrian@gmail.com>
2021-05-25 15:44:19 +0200

    Add Query model

--- api/models.py
+++ api/models.py
@@ -1,12 +1,18 @@
+from sqlalchemy import Column
+from sqlalchemy.orm import relationship
+
 from app import db
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.Text, unique=True)
-    password = db.Column(db.Text)
-    roles = db.Column(db.Text)
-    is_active = db.Column(db.Boolean, default=True, server_default="true")
+    id = Column(db.Integer, primary_key=True)
+    username = Column(db.Text, unique=True)
+    password = Column(db.Text)
+    roles = Column(db.Text)
+    is_active = Column(db.Boolean, default=True, server_default="true")
+    queries = relationship(
+        "Query", back_popupates="user", cascade="all, delete", passive_deletes=True
+    )
 
     @property
     def rolenames(self):
@@ -29,3 +35,10 @@ class User(db.Model):
 
     def is_valid(self):
         return self.is_active
+
+
+class Query(db.Model):
+    id = Column(db.Integer, primary_key=True)
+    user_id = Column(db.Integer, db.ForeignKey("user.id"))
+    name = Column(db.Text)
+    user = relationship("User", back_populates="queries")
