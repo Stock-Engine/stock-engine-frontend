77562ff5f208c0f6faa26d4996ab29a0cc017b30
Adrian Naruszko <naruszko.adrian@gmail.com>
2021-06-01 11:59:08 +0200

    Migrate to DockerHub webhooks

--- ansible/install-docker.sh
+++ ansible/install-docker.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+set -e
+
+CALLBACK_URL=$1
+
 docker-compose pull
 docker-compose up -d --remove-orphans
-docker image prune
+docker image prune -f
+
+curl -X POST -H "Content-Type: application/json" -d '{"state": "success"}' "$CALLBACK_URL"
