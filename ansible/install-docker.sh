b11ad6ab681108dc857aaac3a91e689ae2919df4
Adrian Naruszko <naruszko.adrian@gmail.com>
2021-06-07 15:56:45 +0200

    Add support for staging deployment

--- ansible/install-docker.sh
+++ ansible/install-docker.sh
@@ -3,9 +3,19 @@
 set -e
 
 CALLBACK_URL=$1
+TAG=$2
 
-docker-compose pull
-docker-compose up -d --remove-orphans
+if [ "$TAG" == "stable" ]; then
+  FILE="docker-compose.yml"
+elif [ "$TAG" == "latest" ]; then
+  FILE="docker-compose-staging.yml"
+else
+  echo "Unknown tag $TAG"
+  exit 1
+fi
+
+docker-compose -f "$FILE" pull
+docker-compose -f "$FILE" up -d --remove-orphans
 docker image prune -f
 
 curl -X POST -H "Content-Type: application/json" -d '{"state": "success"}' "$CALLBACK_URL"
